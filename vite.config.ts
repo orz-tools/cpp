b1c9506ee8d18dbd0974a6e73a336a17ef7b0008
Yeechan Lu <git@orzfly.com>
2023-03-12 01:11:13 +0800

    Split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
 })
