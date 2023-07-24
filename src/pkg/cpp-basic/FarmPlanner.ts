980796ab0f9a5cf0284f8fe255b7c3562ed4bd71
Yeechan Lu <git@orzfly.com>
2023-07-24 20:19:57 +0800

    Add minizinc compiler

--- src/pkg/cpp-basic/FarmPlanner.ts
+++ src/pkg/cpp-basic/FarmPlanner.ts
@@ -51,10 +51,12 @@ export class FarmPlanner<G extends IGame> {
         const p = previousResult[`formula:${formula.id}`] || 0
         if (p <= 0) continue
         model.constraints[`formula:${formula.id}`] = { min: 0, max: Math.ceil(p) }
+      } else {
+        model.constraints[`formula:${formula.id}`] = { min: 0 }
       }
 
       const item = {} as Exclude<(typeof model)['variables'][any], undefined>
-      item[`formula:${formula.itemId}`] = 1
+      item[`formula:${formula.id}`] = 1
       item[`item:${formula.itemId}`] = formula.quantity
       for (const cost of formula.costs) {
         item[`item:${cost.itemId}`] = -cost.quantity
@@ -70,6 +72,8 @@ export class FarmPlanner<G extends IGame> {
         const p = previousResult[`battle:${k}`] || 0
         if (p <= 0) continue
         model.constraints[`battle:${k}`] = { min: 0, max: Math.ceil(p) }
+      } else {
+        model.constraints[`battle:${k}`] = { min: 0 }
       }
       model.variables[`battle:${k}`] = Object.assign({}, v.varRow, {
         [`battle:${k}`]: 1,
@@ -141,10 +145,121 @@ export class FarmPlanner<G extends IGame> {
     await Promise.resolve()
     for (const i of this.unfeasible) {
       this.model.variables[`unfeasible:${i}`] = { [i]: 1 }
+      this.model.constraints[`unfeasible:${i}`] = { min: 0 }
     }
     // delete this.model.ints
+    try {
+      console.log('converted as minizinc input', this.compile())
+    } catch (e) {
+      console.warn('failed to convert as minizinc input', e)
+    }
     const result = Lpsolver.Solve(this.model, 1e-4)
     console.log(this.model, result)
     return result
+  }
+
+  public compile() {
+    const varMap = new Map<string, string>()
+    const varSet = new Set()
+    const alloc = function (n: string) {
+      if (varMap.has(n)) return varMap.get(n)!
+      const t = n.replace(/[^a-zA-Z0-9_]/g, '_')
+      if (varSet.has(t)) throw new Error('variable name collision')
+      varSet.add(t)
+      varMap.set(n, t)
+      return t
+    }
+
+    const constraints = new Map<string, { min?: number; max?: number; terms: Map<string, number> }>()
+    const getConstraint = (k: string) => {
+      if (!constraints.has(k)) constraints.set(k, { terms: new Map() })
+      return constraints.get(k)!
+    }
+
+    for (const [k, items] of Object.entries(this.model.variables)) {
+      for (const [kk, vv] of Object.entries(items!)) {
+        if (vv !== 0 && vv !== undefined) {
+          getConstraint(kk).terms.set(k, vv)
+        }
+      }
+    }
+
+    constraints.delete('init')
+
+    const variables = new Map<string, { min?: number; max?: number }>()
+    for (const [label, o] of constraints) {
+      if (o.terms.size === 1 && o.terms.keys().next().value === label) {
+        if (o.terms.values().next().value !== 1) {
+          throw new Error(`invalid self constraint: ${label} = ${o.terms.values().next().value}`)
+        }
+        constraints.delete(label)
+        continue
+      }
+      for (const x of o.terms.keys()) {
+        if (!variables.has(x)) {
+          variables.set(x, {})
+        }
+      }
+    }
+
+    variables.delete('have')
+
+    for (const [key, xx] of Object.entries(this.model.constraints || {})) {
+      const x = xx || {}
+      if (key === 'init') continue
+      if (constraints.has(key)) {
+        const cc = constraints.get(key)!
+        cc.min = x.min
+        cc.max = x.max
+      } else if (variables.has(key)) {
+        const cc = variables.get(key)!
+        cc.min = x.min
+        cc.max = x.max
+      } else {
+        throw new Error('unmigrated constraint: ' + key)
+      }
+    }
+
+    const ap = constraints.get('ap')!
+    if (!ap) throw new Error('target not found: ap')
+    constraints.delete('ap')
+    console.log(constraints, variables)
+
+    const result = []
+    const output = []
+    output.push('"{"')
+    result.push('int: have = 1;')
+    for (const [key, x] of variables) {
+      if (x.min == null && x.max == null) {
+        result.push(`var int: ${alloc(key)};`)
+      } else {
+        result.push(`var ${x.min == null ? '' : x.min}..${x.max == null ? '' : x.max}: ${alloc(key)};`)
+      }
+      output.push(`"${JSON.stringify(key).replaceAll('\\', '\\\\').replaceAll('"', '\\"')}:\\(${alloc(key)}),"`)
+    }
+
+    const buildExpression = (terms: Map<string, number>) => {
+      if (terms.size === 0) return '(0)'
+      const e = []
+      for (const [k, v] of terms) {
+        e.push(`${alloc(k)} * (${v})`)
+      }
+      return '(' + e.join(' + ') + ')'
+    }
+    for (const [, x] of constraints) {
+      const exp = buildExpression(x.terms)
+      if (x.min != null) {
+        result.push(`constraint ${exp} >= ${x.min};`)
+      }
+      if (x.max != null) {
+        result.push(`constraint ${exp} <= ${x.max};`)
+      }
+    }
+    result.push(`solve minimize ${buildExpression(ap.terms)};`)
+    output.push(`"\\"ap\\":\\(${buildExpression(ap.terms)})"`)
+    output.push('"}\\n"')
+    result.push(`output [${output.join(', ')}];`)
+
+    return result.join('\n')
   }
 }
