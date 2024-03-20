f722e7d8c96d7ceafe0153f5ea41b24195622e0e
Yeechan Lu <git@orzfly.com>
2024-03-21 01:59:17 +0800

    More links for re1999

--- src/components/re1999/CharacterContextMenuItems.tsx
+++ src/components/re1999/CharacterContextMenuItems.tsx
@@ -13,6 +13,10 @@ const sanitizeName = (s: string) =>
 export const CharacterContextMenuItems = memo(({ character: char }: { character: ICharacter }) => {
   const character = char as Character
   const gamekee = links.gamekee.find((x) => sanitizeName(x.name) === sanitizeName(character.raw.name))
+  const specialName = { Six: '6', 'Thirty-seven': '37' } as Record<string, string>
+  const nameEng = Object.prototype.hasOwnProperty.call(specialName, character.raw.nameEng)
+    ? specialName[character.raw.nameEng]
+    : character.raw.nameEng
 
   return (
     <>
@@ -35,6 +39,29 @@ export const CharacterContextMenuItems = memo(({ character: char }: { character:
         icon={'search'}
         text={'在「逆流的Rainstorm WIKI攻略组」搜索此角色名'}
         href={`https://reverse1999.gamekee.com/list?kw=${encodeURIComponent(`${character.raw.name}`)}&tab=1`}
+      />
+      <MenuItem
+        {...externalLinkProps}
+        icon={'id-number'}
+        text={'View character profile on Prydwen.gg'}
+        href={`https://www.prydwen.gg/re1999/characters/${encodeURIComponent(
+          `${nameEng}`
+            .replace(/[^A-Za-z0-9]/g, '-')
+            .replace(/-+/g, '-')
+            .replace(/^-|-$/g, '')
+            .toLowerCase(),
+        )}`}
+      />
+      <MenuItem
+        {...externalLinkProps}
+        icon={'id-number'}
+        text={'View character profile on Fandom'}
+        href={`https://reverse1999.fandom.com/wiki/${encodeURIComponent(
+          `${nameEng}`
+            .replace(/[^A-Za-z0-9.-]/g, '_')
+            .replace(/_+/g, '_')
+            .replace(/^_|_$/g, ''),
+        )}`}
       />
     </>
   )
